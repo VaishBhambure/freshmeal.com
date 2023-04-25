132449e4234483ec95b74f6913bff8bde9a38ca0
VaishBhambure <122856710+VaishBhambure@users.noreply.github.com>
2023-04-25 10:35:16 +0530

    Add files via upload
    Creating Hotel based website using HTML,CSS,PHP for database connection

---
+++ products.js
@@ -0,0 +1,99 @@
+// DUMMY FOOD ITEMS (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "Chettinad Fish Fry",
+      desc : "Tamil Nadu's Special Classic Fish Fry.",
+      img : "Chettinad Fish Fry.jpg",
+      price : 300
+    },
+    124: {
+      name : "chicken biryani",
+      desc : "The Best Worldwide & Loved By All Hydrabadi Biryani .",
+      img : "chicken biryani.jpg",
+      price : 350
+    },
+    125: {
+      name : "Tandoori Murgh",
+      desc : "Iconic Smoky Tandoori Murgh.",
+      img : "Tandoori Murgh.jpg",
+      price : 250
+    },
+    126: {
+      name : "Rice Appe",
+      desc : "Paniyaram Helthy Appe. ",
+      img : "Rice Appe.jpg",
+      price : 80
+    },
+    127: {
+      name : "Kadai Paneer",
+      desc : "Soft & Mouth Watering Kadai Paneer. ",
+      img : "Kadai Paneer.jpg",
+      price : 120
+    },
+    128: {
+      name : "paneer tikka",
+      desc : "North Indian-Style Tandoori Paneer Tikka. ",
+      img : "paneer tikka.jpg",
+      price : 150
+    },
+    129: {
+      name : "Uppu huli dosa ",
+      desc : "Special Breakfast Dish of Mangalore & Udapi Crispy Uppu Huli Dosa. ",
+      img : "Uppu huli dosa .jpg",
+      price : 120
+    },
+    130:{
+        name : "Mutton Curry ",
+        desc : "Kolhapuri Special Mutton Curry. ",
+        img : "Mutton Curry.jpg",
+        price : 350
+    },
+    131:{
+        name : "Strawberries Tart",
+        desc : "Juicy Classic French Strawberries Tart. ",
+        img : "Strawberries Tart.jpg",
+        price : 155
+    },
+    132:{
+      name : "one pot pasta",
+      desc : "Tender & Easiest Dinner One Pot Pasta. ",
+      img : "one pot pasta.jpg",
+      price : 100
+  },
+  133:{
+    name : "zucchini and ricotta pizza",
+    desc : "Fantastic Way To Satisfy Your Pizza Carvings . ",
+    img : "zucchini and ricotta pizza.jpg",
+    price : 200
+},
+134:{
+  name : "A peach carrot cake with cream cheese frosting ",
+  desc : "Absolute Best Two Layers Of Tender & Moist Cake . ",
+  img : "A peach carrot cake with cream cheese frosting .jpg",
+  price : 300
+},
+135:{
+  name : "Amrakhand ",
+  desc : "Pure Mango Shrikhanda. ",
+  img : "Amrakhand .jpg",
+  price : 150
+},
+136:{
+  name : "chocolate cupcakes",
+  desc : "The Ultimate Super Moist Chocolate Cupcakes. ",
+  img : "chocolate cupcakes.jpg",
+  price : 200
+},
+137:{
+  name : "spaghetti with tomato",
+  desc : "Slow Cooked Spaghetti With Fresh Tomatoes Sauce. ",
+  img : "spaghetti with tomato.jpg",
+  price : 360
+},
+138:{
+  name : "Sushi",
+  desc : "Popular Japnese Suchi Roll. ",
+  img : "Sushi.jpg",
+  price : 360
+},
+  };
---
+++ welcome.php
@@ -0,0 +1,23 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index1.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <?php echo "<h1>Login successful " . $_SESSION['username'] . "</h1>"; ?>
+    <a href="logout.php">Logout    </a>
+    <a href="index.html"> back to home page to order food</a>
+</body>
+</html>
